0dda931099a8c6594ac00fa77a830d9b34efe2fe
jlsestak <73488851+jlsestak@users.noreply.github.com>
2020-11-21 17:29:28 -0800

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -1,23 +1,22 @@
 
+<!doctype html>
+<html lang="en">
 <?php
 /*
  * Jessica Sestak
- * confirmation.php uploads form information to database
+ * confirmation.php creates a confirmation page to check user information and send it to database
  */
 
-//Will stop post if empty
-if(empty($_POST)) {
-    return;
+//checks to see if form was empty
+if (empty($_POST)) {
+    header("location: index.php");
 }
 
-//set the time zone
-date_default_timezone_set('America/Los_Angeles');
 
-//adds the header and the database credentials 
-include ('includes/head.html');
-require ('includes/dbcreds.php');
+include('includes/head.html');
+require('includes/dbcreds.php');
+require('includes/guestFunctions.php');
 ?>
-
 <body>
 <!-- Jumbotron -->
 <div class="jumbotron jumbotron-fluid">
@@ -27,30 +26,92 @@ require ('includes/dbcreds.php');
 </div>
 <?php
 
+$isValid = true;
 
-//get data from post array
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$job_title = $_POST['jobTitle'];
-$company = $_POST['company'];
-$linkedin = $_POST['link'];
-$email = $_POST['email'];
+
+//Check first name
+if (validName($_POST['fname'])) {
+    $fname = $_POST['fname'];
+} else {
+    echo "<p>Invalid first name</p>";
+    $isValid = false;
+}
+
+//Check last name
+if (validName($_POST['lname'])) {
+    $lname = $_POST['lname'];
+} else {
+    echo "<p>Invalid last name</p>";
+    $isValid = false;
+}
+
+//check meet option
 $meetOption = $_POST['meetSelect'];
-if($meetOption == 'other') {
+if ($meetOption == 'other') {
     $meet = $_POST['meet'];
-}
-else {
+} else if (validMeetSelect($meetOption)) {
     $meet = $_POST['meetSelect'];
+} else {
+    echo "<p>Invalid Meeting</p>";
+    $isValid = false;
 }
-$message = $_POST['comment'];
-if(isset($_POST['test'])) {
-    $mailing = $_POST['mailingSelect'];
-}
-else {
+
+//check mailing
+if (isset($_POST['mailing'])) {
+    if(validMailing($_POST['mailingSelect'])) {
+        $mailing = $_POST['mailingSelect'];
+    }
+    else {
+        echo "<p>Only HTML or Text</p>";
+    }
+} else {
     $mailing = "";
 }
 
-// I use this to check that values are being properly received
+//check email
+$email = "";
+if (isset($_POST['mailing']) and !(validEmail(($_POST['email'])))) {
+    echo "<p>Please put in a valid email</p>";
+    $isValid = false;
+} else {
+    $email = $_POST['email'];
+}
+
+//checked LinkedIn
+$linked = "";
+$linkedCheck = $_POST['link'];
+if ($linkedCheck != "") {
+    if (!(validLinkedin($linkedCheck))) {
+        echo "<p>Please put a valid LinkedIn</p>";
+        $isValid = false;
+    } else {
+        $linked = $linkedCheck;
+    }
+} else {
+    $linked = $linkedCheck;
+}
+
+
+if (!$isValid) {
+    return;
+}
+
+$job_title = $_POST['jobTitle'];
+$company = $_POST['company'];
+$message = $_POST['comment'];
+
+$fname = mysqli_real_escape_string($cnxn, $fname);
+$lname = mysqli_real_escape_string($cnxn, $lname);
+$job_title = mysqli_real_escape_string($cnxn, $job_title);
+$company = mysqli_real_escape_string($cnxn, $company);
+$linked = mysqli_real_escape_string($cnxn, $linked);
+$email = mysqli_real_escape_string($cnxn, $email);
+$meet = mysqli_real_escape_string($cnxn, $meet);
+$message = mysqli_real_escape_string($cnxn, $message);
+$mailing = mysqli_real_escape_string($cnxn, $mailing);
+
+
+//Used for testing
 /*
 echo "<p>Name: $fname $lname</p>";
 echo "<p>Job Title: $job_title</p>";
@@ -61,22 +122,42 @@ echo "<p>Meet: $meet</p>";
 echo "<p>Message: $message</p>";
 echo "<p>Mailing: $mailing</p>";
 var_dump($_POST);
-
 */
 
-    //save order to database
-    $sql= "INSERT INTO guests(fname,lname,job_title,company,
-    linkedin,email,meet,message,mailing)
-    VALUES ('$fname','$lname','$job_title','$company','$linkedin','$email','$meet','$message','$mailing')";
-    $success = mysqli_query($cnxn, $sql);
-    if(!$success) {
-        echo "<p>Sorry... something went wrong.</p>";
-        return;
-    }
+
+//save order to database
+$sql = "INSERT INTO guests(fname,lname,job_title,company,
+linkedin,email,meet,message,mailing)
+ VALUES ('$fname','$lname','$job_title','$company','$linked','$email','$meet','$message','$mailing')";
+$success = mysqli_query($cnxn, $sql);
+
+
+//if doesn't download successfully than prints an error or prints summary if is was successful
+if (!$success) {
+    echo "<p>Sorry... something went wrong.</p>";
+
+} else {
+    echo "<h1 class ='text-center text-white'> Thank you $fname, for filling out Jessica's Guestbook!</h1>";
+    echo "<h4 class = 'text-white ml-5'>Name: $fname $lname</h4>";
+    echo "<h4 class = 'text-white ml-5'>Job Title: $job_title</h4>";
+    echo "<h4 class = 'text-white ml-5'>Company: $company</h4>";
+    echo "<h4 class = 'text-white ml-5'>LinkedIn: $linked</h4>";
+    echo "<h4 class = 'text-white ml-5'>Email: $email</h4>";
+    echo "<h4 class = 'text-white ml-5'>Meet: $meet</h4>";
+    echo "<h4 class = 'text-white ml-5'>Message: $message</h4>";
+    echo "<h4 class = 'text-white ml-5'>Mailing: $mailing</h4>";
+
+}
+
 ?>
+<div class="container text-center">
+    <a href="index.php" class="btn btn-info" role="button">Guestbook Form</a>
+    <a href="adminpage.php" class="btn btn-info" role="button">My Guests</a>
+</div>
+
 
 </body>
-<h1 class ="text-center text-white">Thank you <?php echo $_POST['fname']?>, for filling out Jessica's Guestbook!</h1>
+
 <script crossorigin="anonymous" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script crossorigin="anonymous" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
